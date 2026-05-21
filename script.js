b268ef25dea131a791b0b68cb2b73e310e0ccef3
Bayzid <dbmbayzid00@gmail.com>
2026-05-21 10:52:24 +0600

    added arethmetics operator

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const  courseName = 'ostad-mern-16';
 let isEnrolled = true;
 
 console.log(stdName,age,courseName,isEnrolled);
+
+// Task 2
+let x = 4;
+let y = 6;
+const add = x+y;
+const sub = x - y;
+const mult = x*y;
+const dvs = x/y;
+console.log(add,sub,mult,dvs);
+
